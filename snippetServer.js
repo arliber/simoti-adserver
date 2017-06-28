42fff64b83399fd226d67a285f9d749703ed5f0e
Arik Liber <leonliber@gmail.com>
2017-06-28 18:37:36 +0300

    Refactored server to work with publishers and articles entities

--- snippetServer.js
+++ snippetServer.js
@@ -3,41 +3,110 @@ var datastore = require('@google-cloud/datastore')({
   keyFilename: './keyfile.json'
 });
 
-exports.getSnippetTest = (advId,i) => {
-  console.time('getSnippetTest'+i);
-  return new Promise((resolve, reject) => {
-    var query = datastore.createQuery('snippets')
-                          .filter('advertiserId', '=', advId);
-
-    query.run(function(err, entities, info) {
-      console.timeEnd('getSnippetTest'+i);
-      if(entities.length) {
-        console.log(`getSnippet: Returning snippet [${entities[0][datastore.KEY]}]`)
-        resolve(entities[0]);
-      } else {
-        resolve({ error: 'no snippet' });
-      }
-    });
-  });  
+function getPublisherDomain(url) {
+  const regex = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/ig;
+  let groups = regex.exec(url);
+  if(!groups || groups.length < 2) {
+    return null;
+  } else {
+    return groups[1];
+  }
 }
 
-exports.getSnippet = (url) => {
-  console.time('getSnippet');
-  return new Promise((resolve, reject) => {
-    var query = datastore.createQuery('snippets')
-                         .filter('container.url', '=', url)
-                         .filter('container.status', '=', 'active');
+function getPublisherByDomain(publisherDomain, cb) {
+  let key = datastore.key(['publishers', publisherDomain]);
+  return datastore.get(key);
+}
 
-    query.run(function(err, entities, info) {
-      console.timeEnd('getSnippet');
-      if(entities.length) {
-        console.log(`getSnippet: Returning snippet [${entities[0][datastore.KEY]}]`)
-        resolve(entities[0]);
-      } else {
-        resolve({ error: 'no snippet' });
+function getArtcileId(url, articleIdRegex) {
+  const regex = new RegExp(articleIdRegex, 'ig');
+  let groups = regex.exec(url);
+  if(!groups || groups.length < 2) {
+    return null;
+  } else {
+    return groups[1];
+  }
+}
+
+function getArticleById(publisherId, articleId) {
+  let key = datastore.key(['publishers', publisherId, 'articles', articleId]);
+  return datastore.get(key);
+}
+
+function saveNewArticle(publisherId, articleId) {
+  const key = datastore.key(['publishers', publisherId, 'articles', articleId]);
+  const entity = {
+      key,
+      data: {
+        status: 'pending'
       }
-    });
+  };
+  return datastore.save(entity);
+}
 
+function getOrCreateArticle(publisherId, articleId) {
+  return new Promise((resolve, reject) => {
+    let transaction = datastore.transaction();
+    transaction.run(function(err) {
+      if (err) {
+        console.error(`getOrCreateArticle: Unable to create transaction while processing article [${articleId}]`);
+        reject({error: 'unable to create transaction'});
+      } else {
+        // Search for article
+        getArticleById(publisherId, articleId).then((articles) => {
+          if(articles && articles[0]) { // Exists - return 
+            if(articles[0].status === 'assigned' && articles[0].snippet && articles[0].snippet.status === 'active') {
+              resolve(articles[0].snippet);
+            } else {
+              resolve({});
+            }
+          } else { // Doesn't exist - create
+            saveNewArticle(publisherId, articleId).then((err) => {
+              resolve({});
+            }).catch((err) => {
+              console.error(`getOrCreateArticle: Unable to create a new article with id [${articleId}]`, err);
+              reject({error: `unable to create new article with id [${articleId}]`});
+            }); 
+          }
+        });
+      }// Else
+    }); //Transaction
+  }); // Promise
+} // EOF
+
+exports.getSnippet = (url) => {
+  
+  console.time('getSnippet');
+
+  return new Promise((resolve, reject) => {
+    // Get publisher domain
+    const publisherDomain = getPublisherDomain(url);
+    if(!publisherDomain) {
+      console.error(`getSnippet: Unable to extract publisher name from [${url}]`);
+      resolve({error: 'publisher not found'});
+    } else {
+      // Get publisher entiy
+      getPublisherByDomain(publisherDomain)
+      .then((publisher) => {
+        if(!publisher[0]) {
+          console.time('getSnippet');
+          console.error(`getSnippet: Publisher [${publisherDomain}] was not found`);
+          reject({error: `publisher [${publisherDomain}] does not exist`})
+        } else {
+          // Extract article id
+          let articleId = getArtcileId(url, publisher[0].articleIdRegex);
+          getOrCreateArticle(publisherDomain, articleId).then((res) => {
+            console.time('getSnippet');
+            resolve(res);
+          }).catch((err) =>{
+            console.time('getSnippet');
+            console.error(`getSnippet - unable to process url`, err);
+            reject(err);
+          });
+        }
+      });
+    }
+     
   });
 
 };
--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 const snippetServer = require('./snippetServer');
 
-/*snippetServer.getSnippet('http://www.bizportal.co.il/mutualfunds/news/article/548104').then((entities) => {
+var url = 'http://www.gadgety.co.il/183202/huawei-mate-10-rumors-begin/';
+//var url = 'http://www.bizportal.co.il/mutualfunds/news/article/548104';
+
+snippetServer.getSnippet(url).then((entities) => {
   console.log(entities);
-});*/
-
-for(var i=0;i<100;i++)
-snippetServer.getSnippetTest('123456', i).then((e) => {
-  //console.log(e);
-})
-
+}).catch((err) => {
+  console.error(err);
+});
